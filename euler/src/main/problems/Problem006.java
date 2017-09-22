3b8382c4f1931444726cc1ab56ae398af67bdd25
emulcahy <emulcahy@gmail.com>
2017-09-22 15:30:57 -0400

    easy problems

--- euler/src/main/problems/Problem006.java
+++ euler/src/main/problems/Problem006.java
@@ -4,5 +4,16 @@ package problems;
 //The square of the sum of the first ten natural numbers is, (1 + 2 + ... + 10)^2 = 55^2 = 3025
 //Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
 //Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+
+import java.util.stream.LongStream;
+
 public class Problem006 {
+    public static void main(String[] args) {
+        Long sumOfSquares = LongStream.rangeClosed(1L, 100L)
+                .map(i -> i * i)
+                .sum();
+        Long sumOfFirstHundredNums = LongStream.rangeClosed(1L, 100L).sum();
+        Long squareOfSums = sumOfFirstHundredNums * sumOfFirstHundredNums;
+        System.out.println(squareOfSums - sumOfSquares);
+    }
 }
--- euler/src/main/problems/Problem007.java
+++ euler/src/main/problems/Problem007.java
@@ -1,6 +1,23 @@
 package problems;
 
+import utils.Utils;
+
 //By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
-//What is the 10 001st prime number?
+//What is the 10,001st prime number?
 public class Problem007 {
+    public static void main(String[] args) {
+        long primeIndex = 2;
+        long num = 3;
+
+        while (true) {
+            num += 2;
+            if (Utils.isPrime(num)) {
+                primeIndex++;
+                if (primeIndex == 10001L) {
+                    System.out.println(num + " is the 10,001st prime");
+                    break;
+                }
+            }
+        }
+    }
 }
